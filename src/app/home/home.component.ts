934bea90f5b4a493493560e622f24332dd481d7d
victor <gfeng005@uottawa.ca>
2025-03-01 21:03:59 -0500

    created computed signal for beginner courses and advanced courses in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import {Component, signal} from '@angular/core';
-import {Course} from "../models/course.model";
+import {Component, computed, effect, signal} from '@angular/core';
+import {Course, sortCoursesBySeqNo} from "../models/course.model";
 import {MatTab, MatTabGroup} from "@angular/material/tabs";
 import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
 import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
     selector: 'home',
@@ -16,18 +17,26 @@ import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
 })
 export class HomeComponent {
 
-    courses = signal<Course[]>([]);
+    #courses = signal<Course[]>([]);
 
-    constructor(private coursesService: CoursesServiceWithFetch) {
+    beginnerCourses = computed(() => this.#courses().filter(course => course.category === 'BEGINNER'));
+    advancedCourses = computed(() => this.#courses().filter(course => course.category === 'ADVANCED'));
+
+    constructor(private coursesService: CoursesService) {
+
+        effect(() => {
+            console.log('Beginner courses', this.beginnerCourses());
+            console.log('Advanced courses', this.advancedCourses());
+        });
         this.loadCourses().then(() => {
-            console.log("Courses loaded");
+            console.log('Courses loaded:' , this.#courses());
         });
     }
 
     async loadCourses() {
         try{
             const courses = await this.coursesService.loadAllCourses();
-            this.courses.set(courses);
+            this.#courses.set(courses.sort(sortCoursesBySeqNo));
         }
         catch(error) {
             alert("Error loading courses");
