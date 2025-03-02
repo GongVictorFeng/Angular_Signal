2ae24cfad3fa1d5123dc871a2dfc9cde825d136d
victor <gfeng005@uottawa.ca>
2025-03-01 20:19:01 -0500

    created the loadCourses method in home component to get all courses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,8 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
+import {Component, signal} from '@angular/core';
+import {Course} from "../models/course.model";
 import {MatTab, MatTabGroup} from "@angular/material/tabs";
 import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
 
 @Component({
     selector: 'home',
@@ -20,4 +16,22 @@ import {toObservable, toSignal, outputToObservable, outputFromObservable} from "
 })
 export class HomeComponent {
 
+    courses = signal<Course[]>([]);
+
+    constructor(private coursesService: CoursesServiceWithFetch) {
+        this.loadCourses().then(() => {
+            console.log("Courses loaded");
+        });
+    }
+
+    async loadCourses() {
+        try{
+            const courses = await this.coursesService.loadAllCourses();
+            this.courses.set(courses);
+        }
+        catch(error) {
+            alert("Error loading courses");
+            console.error("Error loading courses", error);
+        }
+    }
 }
