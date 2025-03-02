666ebb0e733dcd5e00a7f8ea97f5cff01cdd9e18
victor <gfeng005@uottawa.ca>
2025-03-02 15:14:02 -0500

    created the skeleton of the edit course dialog.

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,12 +1,13 @@
-import {Component, effect, inject, signal} from '@angular/core';
+import {Component, effect, Inject, inject, OnInit, signal} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material/dialog";
 import {Course} from "../models/course.model";
 import {EditCourseDialogData} from "./edit-course-dialog.data.model";
 import {CoursesService} from "../services/courses.service";
 import {LoadingIndicatorComponent} from "../loading/loading.component";
-import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {CourseCategoryComboboxComponent} from "../course-category-combobox/course-category-combobox.component";
 import {CourseCategory} from "../models/course-category.model";
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'edit-course-dialog',
@@ -19,7 +20,40 @@ import {CourseCategory} from "../models/course-category.model";
   templateUrl: './edit-course-dialog.component.html',
   styleUrl: './edit-course-dialog.component.scss'
 })
-export class EditCourseDialogComponent {
+export class EditCourseDialogComponent implements OnInit {
 
+  form!: FormGroup;
+
+  constructor(
+      private dialog: MatDialog, 
+      private dialogRef: MatDialogRef<EditCourseDialogComponent>,
+      @Inject(MAT_DIALOG_DATA) public data: EditCourseDialogData,
+      private fb: FormBuilder
+    ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      title: this.data.course?.title,
+      longDescription: this.data.course?.longDescription,
+      category: this.data.course?.category,
+      iconUrl: this.data.course?.iconUrl
+    })
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
 
 }
+
+export async function openEditCourseDialog(dialog: MatDialog, data: EditCourseDialogData) {
+  const config = new MatDialogConfig();
+  config.disableClose = true;
+  config.autoFocus = true;
+  config.width = "400px";
+  config.data = data;
+
+  const close$ = dialog.open(EditCourseDialogComponent, config).afterClosed();
+  return firstValueFrom(close$)
+}
+
