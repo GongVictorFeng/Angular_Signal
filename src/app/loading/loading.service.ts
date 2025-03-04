804a8ca5671015be95d86c1f355d118e756d0dcc
victor <gfeng005@uottawa.ca>
2025-03-04 09:52:06 -0500

    created the loading service

--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -1,8 +1,19 @@
-import {Injectable, signal} from "@angular/core";
+import {Injectable, Signal, signal, WritableSignal} from "@angular/core";
 
 @Injectable({
   providedIn: "root",
 })
 export class LoadingService {
 
+  #loadingSignal: WritableSignal<boolean> = signal(false);
+
+  loading = this.#loadingSignal.asReadonly()
+
+  loadingOn() {
+    this.#loadingSignal.set(true);
+  }
+
+  loadingOff() {
+    this.#loadingSignal.set(false);
+  }
 }
