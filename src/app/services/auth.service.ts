9da4fe9f313736e728650c6be711b205eeacef5f
victor <gfeng005@uottawa.ca>
2025-03-05 11:17:43 -0500

    created the authentication service.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import {computed, effect, inject, Injectable, signal} from "@angular/core";
 import {User} from "../models/user.model";
 import {environment} from "../../environments/environment";
 import {Router} from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { firstValueFrom } from "rxjs";
 
 const USER_STORAGE_KEY = 'user';
 
@@ -10,5 +12,22 @@ const USER_STORAGE_KEY = 'user';
 })
 export class AuthService {
 
+  #userSignal = signal<User | null>(null);
 
+  user = this.#userSignal.asReadonly();
+
+  isLoggedIn = computed(() => !!this.user());
+
+  constructor(private httpService: HttpClient){}
+
+  async login(email: string, password: string): Promise<User> {
+    const user$ = this.httpService.post<User>(`${environment.apiRoot}/login`, {
+      email,
+      password
+    });
+
+    const user = await firstValueFrom(user$);
+    this.#userSignal.set(user);
+    return user;
+  }
 }
