6cf60f6f57f478f9ae32ea4a86cca3932c25f76f
victor <gfeng005@uottawa.ca>
2025-03-05 11:18:05 -0500

    completed the login component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import {Component, inject} from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
 import {AuthService} from "../services/auth.service";
 import {MessagesService} from "../messages/messages.service";
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
     selector: 'login',
@@ -15,5 +15,30 @@ import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 })
 export class LoginComponent {
 
+    form !: FormGroup;
 
+    constructor(
+        private fb: FormBuilder, 
+        private messageService: MessagesService, 
+        private authService: AuthService,
+        private router: Router)
+    {
+        this.form = this.fb.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required]
+        })
+    }
+
+    async onLogin() {
+        try {
+            const {email, password} = this.form.value;
+            await this.authService.login(email, password);
+            await this.router.navigate(['/home']);
+        } 
+        catch (err) {
+            this.messageService.showMessage("failed to login, please try again", 'error');
+            console.error(err)
+        }
+    }
+    
 }
