b2794059dba9d87cc5882d51bbd851d94be4f91f
victor <gfeng005@uottawa.ca>
2025-03-05 09:15:41 -0500

    created the message component.

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,6 +1,7 @@
-import {Component, inject} from "@angular/core";
+import {Component, Signal} from "@angular/core";
 import {MessagesService} from "./messages.service";
 import {NgClass} from "@angular/common";
+import { Message } from "../models/message.model";
 
 @Component({
     selector: 'messages',
@@ -12,5 +13,14 @@ import {NgClass} from "@angular/common";
 })
 export class MessagesComponent {
 
+    message!: Signal<Message | null>;
+
+    constructor(private messageService: MessagesService) {
+        this.message = this.messageService.message
+    };
+
+    onClose() {
+        this.messageService.clear();
+    }
 
 }
