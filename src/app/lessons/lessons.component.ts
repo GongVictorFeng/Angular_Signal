596e42be8168673c77bcab77acc4cae6e0d0c865
victor <gfeng005@uottawa.ca>
2025-03-08 11:13:20 -0500

    created the lessons component by implementing the master detail pattern.

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, inject, signal, viewChild} from '@angular/core';
+import {Component, ElementRef, signal, viewChild} from '@angular/core';
 import {LessonsService} from "../services/lessons.service";
 import {Lesson} from "../models/lesson.model";
 import {LessonDetailComponent} from "./lesson-detail/lesson-detail.component";
@@ -13,7 +13,29 @@ import {LessonDetailComponent} from "./lesson-detail/lesson-detail.component";
 })
 export class LessonsComponent {
 
+    mode = signal<'master' | 'detail'>('master');
+    lessons = signal<Lesson[]>([]);
+    selectedLesson = signal<Lesson | null>(null)
+    searchInput = viewChild.required<ElementRef>('search');
 
+    constructor(private lessonsService: LessonsService){}
 
+    async onSearch() {
+        const query = this.searchInput()?.nativeElement.value;
+        this.lessons.set(await this.lessonsService.loadLessons({query}));
+    }
 
+    onLessonSelected(lesson: Lesson) {
+        this.mode.set('detail');
+        this.selectedLesson.set(lesson);
+    }
+
+    onCancel() {
+        this.mode.set('master');
+    }
+
+    onLessonUpdated(updatedLesson: Lesson) {
+        this.lessons.update(lessons => lessons.map(lesson => lesson.id === updatedLesson.id ? updatedLesson: lesson));
+        this.mode.set('master');
+    }
 }
